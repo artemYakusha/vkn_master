48d44521b548744a26fad7dd8fadbcf4e23d9ca6
artemYakusha <111080434+artemYakusha@users.noreply.github.com>
2022-09-22 16:46:29 +0300

    labs 1 2 5

--- lab5.py
+++ lab5.py
@@ -17,3 +17,31 @@ elif "второе условие":
 else:
     y = f3(bar)
 print(int(y))
+
+#5.2
+a1 = 7
+a2 = 10
+a3 = 4
+bar = int(input('Введіть N  '))
+foo1 = bar/a1
+Foo1 = foo1 / (int(foo1))
+foo2 = bar/a2
+Foo2 = foo2 / (int(foo2))
+foo3 = bar/a3
+Foo3 = foo3 / (int(foo3))
+if Foo1 == 1 and Foo2 == 1 and Foo3 == 1:
+    print('це спільне кратне для всіх чисел')
+elif Foo1 == 1 and Foo2 == 1:
+    print('це спільне кратне тільки для а1 та а2')
+elif Foo1 == 1 and Foo3 == 1:
+    print("це спільне кратне тільки для а1 та а3")
+elif Foo2 == 1 and Foo3 == 1:
+    print("це спільне кратне тільки для а2 та а3")
+elif Foo1 == 1:
+    print("це спільне кратне тільки для а1")
+elif Foo2 == 1:
+    print("це спільне кратне тільки для а2")
+elif Foo3 == 1:
+    print("це спільне кратне тільки для а3")
+else:
+    print('це не є спільне кратне для жодного числа')
