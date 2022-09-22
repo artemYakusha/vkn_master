046fdc49c4e6431aef85f5b67ebcaacdb9ed2b59
artemYakusha <111080434+artemYakusha@users.noreply.github.com>
2022-09-22 15:31:22 +0300

    lab5.1

---
+++ lab5.py
@@ -0,0 +1,19 @@
+import math
+bar = float(input('Введіть число х:  '))
+def f1(bar1):
+    rez = "тут первый выраз системы"
+    return (rez)
+def f2(bar2):
+    rez = "тут второй"
+    return (rez)
+def f3(bar3):
+    rez = "тут третий"
+    return (rez)
+y = 0.0
+if "первое условия":
+    y = f1(bar)
+elif "второе условие":
+    y = f2(bar)
+else:
+    y = f3(bar)
+print(int(y))
